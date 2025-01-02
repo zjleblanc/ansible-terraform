dfd2a8bd617b7ac954944152693232b12ea13c4e
Zachary LeBlanc <zjleblanc3@gmail.com>
2025-01-02 13:06:59 -0600

    use az cli env vars globally

--- create_remote_storage/providers.tf
+++ create_remote_storage/providers.tf
@@ -13,8 +13,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = var.az_client_id
-  client_secret   = var.az_client_secret
-  tenant_id       = var.az_tenant
-  subscription_id = var.az_subscription
+  # client_id       = var.az_client_id
+  # client_secret   = var.az_client_secret
+  # tenant_id       = var.az_tenant
+  # subscription_id = var.az_subscription
 }
--- create_remote_storage/variables.tf
+++ create_remote_storage/variables.tf
@@ -1,7 +1,7 @@
-variable "az_client_id" {}
-variable "az_client_secret" {}
-variable "az_tenant" {}
-variable "az_subscription" {}
+# variable "az_client_id" {}
+# variable "az_client_secret" {}
+# variable "az_tenant" {}
+# variable "az_subscription" {}
 
 variable "az_resource_group" { default = "openenv-jmdmt" }
 variable "az_resource_group_loc" { default = "eastus" }
